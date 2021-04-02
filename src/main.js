19c3742562a9802ff47df774f4ef59beaf236d91
martinesidens <martines.idens@gmail.com>
2021-04-02 09:59:59 +0300

    02.04.2021 10:00

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
+
+import {setTripControls} from './view/stats.js';
+import {setTripFilters} from './view/filters.js';
+import {setTripInfo} from './view/trip-info.js';
+import {setNewPointTrip} from './view/newPoint.js';
+import {setSortDay} from './view/sort-day.js';
+import {setEditPoint} from './view/edit-point.js'
+
+const tripControls = document.querySelector('.trip-controls');
+const tripFilters = document.querySelector('.trip-controls__filters');
+const tripInfo = document.querySelector('.trip-main');
+const sortDay = document.querySelector('.trip-events');
+const editPoint = document.querySelector('.event');
+
 function render  (container, template, place) {
-  container.insertAdjacentHTML(place, template);
+container.insertAdjacentHTML(place, template);
 };
+
+render(tripControls, setTripControls(), 'afterbegin');
+render(tripFilters, setTripFilters(), 'beforeend');
+render(tripInfo, setTripInfo(), 'afterbegin');
+render(sortDay, setSortDay(), 'afterbegin');
+// render(addNewPoint, setNewPointTrip(), 'afterbegin');
+render(sortDay, setEditPoint(), 'beforeend');
+render(editPoint, setNewPointTrip(), 'beforeend');
---
+++ src/view/stats.js
@@ -0,0 +1,12 @@
+function setTripControls () {
+  return `<div class="trip-main__trip-controls  trip-controls">
+  <div class="trip-controls__navigation">
+    <h2 class="visually-hidden">Switch trip view</h2>
+    <nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn" href="#">Table</a>
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a>
+    </nav>
+  </div>`
+}
+
+export {setTripControls};
