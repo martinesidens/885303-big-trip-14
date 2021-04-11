fd9d2fa1a1564581de5e62c8cb042b8c46489da3
martinesidens <martines.idens@gmail.com>
2021-04-11 13:25:24 +0300

    11.04.2021 13:30

---
+++ src/mock/points.js
@@ -0,0 +1,17 @@
+
+const pointType = ['Taxi', 'Bus', 'Train', 'Ship', 'Transport', 'Drive', 'Flight', 'Check-in', 'Sightseeng', 'Restaurant'];
+const destinationTrip = ['Genova', 'Milan', 'Torino', 'Como', 'Lugano'];
+
+function generatePoint () {
+  return {
+    pointType: pointType[i],
+    destination: destinationTrip[i],
+    dataStartTrip: '',
+    dataEndTrip: '',
+    price: 100,
+
+    offers: {
+
+    }
+  }
+}
