0f7a95707b82a2466b6da5ce2e85672c9fcc5a73
MartinIdens <martines.idens@gmail.com>
2021-05-17 21:32:27 +0300

    17.05.2021 21:35

--- src/view/empty-list.js
+++ src/view/empty-list.js
@@ -1,27 +1,11 @@
-import {createElement} from "../util";
+import AbstractView from './abstract.js';
 
 function generationEmptyList () {
   return '<h2 class="visually-hidden">Trip events</h2><p class="trip-events__msg">Click New Event to create your first point</p>'
 }
 
-export default class EmptyList {
-  constructor(data) {
-    this._element = null;
-    this._data = data;
-  }
-
+export default class EmptyList extends AbstractView {
   getTemplate() {
     return generationEmptyList(this._data);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement (this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
